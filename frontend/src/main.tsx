d6526ba3b80c8f43e1c41d3d76e787c3ec8eff23
gitADITYAhub <adityasinghhvm10@gmail.com>
2024-03-03 21:52:59 +0530

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+)
